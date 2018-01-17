797515340f635f4af0436492c605e714991f9467
Patrik Smeds <patrik.smeds@gmail.com>
2018-01-17 09:37:26 +0000

    Updated code to work with kibana 6.1.1

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-'use strict';
-
-module.exports = function (kibana) {
-
-  return new kibana.Plugin({
-
-    uiExports: {
-      visTypes: ['plugins/traffic_sg/traffic_sg']
-    }
-
-  });
-};
+export default kibana => new kibana.Plugin({
+  require: ['kibana', 'elasticsearch'],
+  uiExports: {
+    visTypes: [
+      'plugins/traffic_sg/traffic_sg'
+    ]
+  }
+});
--- public/traffic_sg.js
+++ public/traffic_sg.js
@@ -1,62 +1,67 @@
 // we need to load the css ourselves
-import 'plugins/traffic_sg/traffic_sg.less';
-import 'plugins/traffic_sg/traffic_sg_controller';
-import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+//import 'plugins/traffic_sg/traffic_sg.less';
+//import 'plugins/traffic_sg/traffic_sg_controller';
+import { CATEGORY } from 'ui/vis/vis_category';
+import { VisFactoryProvider } from 'ui/vis/vis_factory';
 import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
-import { VisSchemasProvider } from 'ui/vis/schemas';
-import trafficVisTemplate from 'plugins/traffic_sg/traffic_sg.html';
-import trafficVisParamsTemplate from 'plugins/traffic_sg/traffic_sg_params.html';
+//import { VisSchemasProvider } from 'ui/vis/editors/default/schemas';
+//import trafficVisTemplate from 'plugins/traffic_sg/traffic_sg.html';
+//import trafficVisParamsTemplate from 'plugins/traffic_sg/traffic_sg_params.html';
+import image from '../screenshot.png';
 
-
-// register the provider with the visTypes registry
-VisTypesRegistryProvider.register(MetricVisProvider);
-
-function MetricVisProvider(Private) {
-  var TemplateVisType = Private(TemplateVisTypeProvider);
-  var Schemas = Private(VisSchemasProvider);
+const MetricVisType = (Private) => {
+  const VisFactory = Private(VisFactoryProvider);
+  //const Schemas = Private(VisSchemasProvider);
 
   // return the visType object, which kibana will use to display and configure new
   // Vis object of this type.
-  return new TemplateVisType({
+  return VisFactory.createBaseVisualization({
     name: 'traffic',
     title: 'Traffic',
+    image,
     description: 'Chart display lights of a standard color green/yellow/red',
+    category: CATEGORY.OTHER,
     icon: 'fa-thumbs-up',
-    template: trafficVisTemplate,
-    params: {
-      defaults: {
-        titleTraffic: null,
-        fontSize: 60,
-        width: 50,
-        redThreshold: 20,
-        greenThreshold: 80,
-        invertScale: null,
-        handleNoResults: true
-      },
-      editor: trafficVisParamsTemplate
-    },
-    schemas: new Schemas([
-      {
-        group: 'metrics',
-        name: 'metric',
-        title: 'Metric',
-        min: 1,
-        max: 1,
-        defaults: [
-          { type: 'count', schema: 'metric' }
-        ]
-      },
-      {
-        group: 'buckets',
-        name: 'segment',
-        title: 'X-Axis',
-        min: 0,
-        max: 1,
-        aggFilter: ['terms']
-      }
-    ])
+    //template: trafficVisTemplate,
+    //params: {
+    //  defaults: {
+    //    titleTraffic: null,
+    //    fontSize: 60,
+    //    width: 50,
+    //    redThreshold: 20,
+    //    greenThreshold: 80,
+    //    invertScale: null,
+    //    handleNoResults: true
+    //  },
+    //  editor: trafficVisParamsTemplate
+    //},
+    //editorConfig: {
+    //  collections: {},
+    //	schemas: new Schemas([
+    //  	{
+    //    	group: 'metrics',
+    //    	name: 'metric',
+    //    	title: 'Metric',
+    //    	min: 1,
+    //    	max: 1,
+    //    	defaults: [
+    //      	{ type: 'count', schema: 'metric' }
+    //    	]
+    // 	},
+    //  	{
+    //    	group: 'buckets',
+    //    	name: 'segment',
+    //    	title: 'X-Axis',
+    //    	min: 0,
+    //    	max: 1,
+    //    	aggFilter: ['terms']
+    //  	}
+   // 	])
+   //  }
   });
 }
 
 // export the provider so that the visType can be required with Private()
-export default MetricVisProvider;
+//export default MetricVisProvider;
+// register the provider with the visTypes registry
+VisTypesRegistryProvider.register(MetricVisType);
